e150bb40caeca9adee5ceff4307f45b6a663ece0
Chris Cheong <chrisxcheong@gmail.com>
2019-07-11 07:19:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(katz_deli)
   if katz_deli.size == 0
-    puts "The line is currently empty."
+    return puts "The line is currently empty."
   else
     line = katz_deli.map.with_index {|name, index| " #{index+1}.#{name}"}
     puts "The line is currently #{line.join}"
