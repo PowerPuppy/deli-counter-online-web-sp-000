e72b4c35031907dca86755111fd209effbc5ca52
Chris Cheong <chrisxcheong@gmail.com>
2019-07-11 02:47:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
-  else katz_deli.each {|name| position =  }
+  else katz_deli.each {|name|}
      "The line is currently #{katz_deli.size+1}."
   end
 
