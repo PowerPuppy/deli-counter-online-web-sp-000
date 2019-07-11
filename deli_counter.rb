68b29c113c8cf073a0e8692f88919e8df6a3e88b
Chris Cheong <chrisxcheong@gmail.com>
2019-07-11 00:28:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,14 @@
 # Write your code here.
-def line(array)
-  if array.size == o
+def line(katz_deli)
+  if katz_deli.size == o
     puts "This line is currently empty"
-  else puts "Your current place in line is #{array.size+1}."
+  else puts "Your current place in line is #{katz_deli.size+1}."
   end
+
+end
+
+def take_a_number(katz_deli, name)
+  name << katz_deli
+  puts "#{name} you are #{katz_deli.size+1} in line"
 
 end
